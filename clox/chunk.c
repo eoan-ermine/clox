9350cef03e22ea554c75500f529bbc4a249b08e3
Danil Sidoruk <danilsidoruknv@yandex.ru>
2023-10-18 14:54:41 +0300

    Use RLE for storing lines information

--- clox/chunk.c
+++ clox/chunk.c
@@ -5,13 +5,13 @@ void initChunk(Chunk *chunk) {
   chunk->count = 0;
   chunk->capacity = 0;
   chunk->code = NULL;
-  chunk->lines = NULL;
+  initRLEArray(&chunk->lines);
   initValueArray(&chunk->constants);
 }
 
 void freeChunk(Chunk *chunk) {
   FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
-  FREE_ARRAY(int, chunk->lines, chunk->capacity);
+  initRLEArray(&chunk->lines);
   freeValueArray(&chunk->constants);
   initChunk(chunk);
 }
@@ -22,15 +22,18 @@ void writeChunk(Chunk *chunk, uint8_t byte, int line) {
     chunk->capacity = GROW_CAPACITY(oldCapacity);
     chunk->code =
         GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
-    chunk->lines = GROW_ARRAY(int, chunk->lines, oldCapacity, chunk->capacity);
   }
 
   chunk->code[chunk->count] = byte;
-  chunk->lines[chunk->count] = line;
+  writeRLEArray(&chunk->lines, line);
   chunk->count++;
 }
 
 int addConstant(Chunk *chunk, Value value) {
   writeValueArray(&chunk->constants, value);
   return chunk->constants.count - 1;
+}
+
+int getLine(Chunk *chunk, int instr_idx) {
+  return getRLEArray(&chunk->lines, instr_idx);
 }
--- clox/chunk.h
+++ clox/chunk.h
@@ -2,6 +2,7 @@
 #define clox_chunk_h
 
 #include "common.h"
+#include "rle.h"
 #include "value.h"
 
 typedef enum {
@@ -13,7 +14,7 @@ typedef struct {
   int count;
   int capacity;
   uint8_t *code;
-  int *lines;
+  RLEArray lines;
   ValueArray constants;
 } Chunk;
 
@@ -21,5 +22,6 @@ void initChunk(Chunk *chunk);
 void freeChunk(Chunk *chunk);
 void writeChunk(Chunk *chunk, uint8_t byte, int line);
 int addConstant(Chunk *chunk, Value value);
+int getLine(Chunk *chunk, int instr_idx);
 
 #endif
