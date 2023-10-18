781731fba606e854dcf2d3c87ab0b95f272bb613
Danil Sidoruk <danilsidoruknv@yandex.ru>
2023-10-18 15:51:50 +0300

    Implement DEBUG_TRACE_EXECUTION flag

--- clox/main.c
+++ clox/main.c
@@ -1,6 +1,4 @@
 #include "chunk.h"
-#include "common.h"
-#include "debug.h"
 #include "vm.h"
 
 int main(int argc, const char *argv[]) {
@@ -12,10 +10,8 @@ int main(int argc, const char *argv[]) {
   int constant = addConstant(&chunk, 1.2);
   writeChunk(&chunk, OP_CONSTANT, 123);
   writeChunk(&chunk, constant, 123);
-
   writeChunk(&chunk, OP_RETURN, 123);
 
-  disassembleChunk(&chunk, "test chunk");
   interpret(&chunk);
   freeVM();
   freeChunk(&chunk);
