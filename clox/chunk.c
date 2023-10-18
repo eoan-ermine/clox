dff8c2b745fc4824d7b3ebf8dd3c14cba366b36c
Danil Sidoruk <danilsidoruknv@yandex.ru>
2023-10-18 14:05:49 +0300

    Fix formatting

--- clox/chunk.c
+++ clox/chunk.c
@@ -22,8 +22,7 @@ void writeChunk(Chunk *chunk, uint8_t byte, int line) {
     chunk->capacity = GROW_CAPACITY(oldCapacity);
     chunk->code =
         GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
-    chunk->lines =
-        GROW_ARRAY(int, chunk->lines, oldCapacity, chunk->capacity);
+    chunk->lines = GROW_ARRAY(int, chunk->lines, oldCapacity, chunk->capacity);
   }
 
   chunk->code[chunk->count] = byte;
@@ -31,7 +30,7 @@ void writeChunk(Chunk *chunk, uint8_t byte, int line) {
   chunk->count++;
 }
 
-int addConstant(Chunk* chunk, Value value) {
+int addConstant(Chunk *chunk, Value value) {
   writeValueArray(&chunk->constants, value);
   return chunk->constants.count - 1;
 }
