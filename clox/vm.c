0d55f5aa2e6fbf0f6e19bb3b10c8987c4a50c4a9
Danil Sidoruk <danilsidoruknv@yandex.ru>
2023-10-20 14:36:44 +0300

    Implement basic scanning

--- clox/vm.c
+++ clox/vm.c
@@ -2,6 +2,7 @@
 
 #include "chunk.h"
 #include "common.h"
+#include "compiler.h"
 #include "debug.h"
 #include "vm.h"
 
@@ -82,8 +83,7 @@ static InterpretResult run() {
 #undef BINIARY
 }
 
-InterpretResult interpret(Chunk *chunk) {
-  vm.chunk = chunk;
-  vm.ip = vm.chunk->code;
-  return run();
+InterpretResult interpret(const char *source) {
+  compile(source);
+  return INTERPRET_OK;
 }
