4a5ffdb3fbaa720f9e85f5571967524bca56439b
Danil Sidoruk <danilsidoruknv@yandex.ru>
2023-10-31 17:21:53 +0300

    Implement variable declarations

--- clox/vm.c
+++ clox/vm.c
@@ -8,6 +8,7 @@
 #include "debug.h"
 #include "memory.h"
 #include "object.h"
+#include "table.h"
 #include "value.h"
 #include "vm.h"
 
@@ -30,10 +31,12 @@ static void runtimeError(const char *format, ...) {
 void initVM() {
   resetStack();
   vm.objects = NULL;
+  initTable(&vm.globals);
   initTable(&vm.strings);
 }
 
 void freeVM() {
+  initTable(&vm.globals);
   freeTable(&vm.strings);
   freeObjects();
 }
@@ -72,6 +75,7 @@ void replace(Value value) { *(vm.stackTop - 1) = value; }
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define READ_STRING() AS_STRING(READ_CONSTANT())
 #define BINARY_OP(valueType, op)                                               \
   do {                                                                         \
     if (!IS_NUMBER(peek(0)) || !IS_NUMBER(peek(1))) {                          \
@@ -113,6 +117,12 @@ static InterpretResult run() {
     case OP_POP:
       pop();
       break;
+    case OP_DEFINE_GLOBAL: {
+      ObjString *name = READ_STRING();
+      tableSet(&vm.globals, name, peek(0));
+      pop();
+      break;
+    }
     case OP_EQUAL: {
       Value b = peek(1);
       replace(BOOL_VAL(valuesEqual(peek(0), b)));
@@ -165,6 +175,7 @@ static InterpretResult run() {
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef READ_STRING
 #undef BINIARY
 }
 
