8a571ffcaefb74c1d59513b4532e7b12218c2ba4
Danil Sidoruk <danilsidoruknv@yandex.ru>
2024-01-08 22:12:27 +0300

    [25.4.4] Closing upvalues at runtime

--- clox/vm.c
+++ clox/vm.c
@@ -166,6 +166,15 @@ static ObjUpvalue *captureUpvalue(Value *local) {
   return newUpval;
 }
 
+static void closeUpvalues(Value *last) {
+  while (vm.openUpvalues != NULL && vm.openUpvalues->location >= last) {
+    ObjUpvalue *upvalue = vm.openUpvalues;
+    upvalue->closed = *upvalue->location;
+    upvalue->location = &upvalue->closed;
+    vm.openUpvalues = upvalue->next;
+  }
+}
+
 static InterpretResult run() {
   CallFrame *frame = &vm.frames[vm.frameCount - 1];
 
@@ -352,8 +361,13 @@ static InterpretResult run() {
       }
       break;
     }
+    case OP_CLOSE_UPVALUE:
+      closeUpvalues(vm.stackTop - 1);
+      pop();
+      break;
     case OP_RETURN: {
       Value result = pop();
+      closeUpvalues(frame->slots);
       vm.frameCount--;
       if (vm.frameCount == 0) {
         pop();
